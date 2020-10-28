4b721910076c87ea4eb178950b50665ec2621bae
jaycariaga <jaymcariaga@gmail.com>
2020-10-28 14:17:59 -0400

    idk man

--- Project 3/vencrypt.py
+++ Project 3/vencrypt.py
@@ -2,25 +2,29 @@ from sys import argv;
 #Author: Jason Cariaga
 #171001720
 try:
-	plaintxt = argv[2];
+	plainfile = argv[2];
 	cipherfile = argv[3];
 	keyfile = argv[1]
 	#ciphertxt = open(cipherfile)
 	keytxt = open(keyfile)
+	plaintxt = open(plainfile)
+	print(ord('A'))
 except:
 	print("please enter in values for keyfile, plaintext and ciphertext");
 	exit();
 
 bytekey = bytearray(keytxt.read(), 'utf-8')
-bytetxt = bytearray(plaintxt, 'utf-8')
+bytetxt = bytearray(plaintxt.read(), 'utf-8')
+print(bytetxt)
+print(bytekey)
 result = []
 #to handle encrypting the plaintext and returning the ciphertext
-for item in range(len(plaintxt)):
+for item in range(len(bytetxt)):
 	print(bytetxt[item])
-	print(bytekey[item])
+	#print(bytekey[item])
 	temp = (bytetxt[item] + bytekey[item]) % 256
-	print(temp)
-	#temp += bytearray(b"A")[0]
+	#print(temp)
+	###temp += bytearray(b"A")[0]
 	result.append(temp)
 
 ciphertxt = open(cipherfile, "w")

b67ac22cde28ba0876800f9522f72e1324ad0360
jaycariaga <jaymcariaga@gmail.com>
2020-10-28 17:13:00 -0400

    yea dog idk man

--- Project 3/vencrypt.py
+++ Project 3/vencrypt.py
@@ -5,31 +5,61 @@ try:
 	plainfile = argv[2];
 	cipherfile = argv[3];
 	keyfile = argv[1]
-	#ciphertxt = open(cipherfile)
-	keytxt = open(keyfile)
-	plaintxt = open(plainfile)
-	print(ord('A'))
+	#keytxt = open(keyfile, "rb") 
+	# #plaintxt = open(plainfile, "rb")
 except:
 	print("please enter in values for keyfile, plaintext and ciphertext");
 	exit();
 
-bytekey = bytearray(keytxt.read(), 'utf-8')
-bytetxt = bytearray(plaintxt.read(), 'utf-8')
+
+bytekey = []
+bytetxt = []
+with open(keyfile, "rb") as keytxt: #keytxt = open(keyfile, "rb")
+		while True:
+			byte = keytxt.read(1);
+			if not byte:
+				break;
+			bytekey.append(byte);
+
+with open(plainfile, "rb") as plaintxt: #keytxt = open(keyfile, "rb")
+	while True:
+		byte = plaintxt.read(1);
+		if not byte:
+			break;
+		bytetxt.append(byte);  
+	#plaintxt = open(plainfile, "rb")
+
+#actually works and covers the case of a null key LMAO
+while(len(bytekey) < len(bytetxt)):
+	i = 0;
+	if not bytekey:
+		bytekey.append('\0')
+	bytekey.append(bytekey[i])
+	i += 1;
+
+#converts below to ascii number from 0-255
+
+#bytekey = bytearray(keytxt.read(), 'utf-8')
+#bytetxt = bytearray(plaintxt.read(), 'utf-8')
 print(bytetxt)
 print(bytekey)
 result = []
 #to handle encrypting the plaintext and returning the ciphertext
-for item in range(len(bytetxt)):
-	print(bytetxt[item])
-	#print(bytekey[item])
-	temp = (bytetxt[item] + bytekey[item]) % 256
-	#print(temp)
-	###temp += bytearray(b"A")[0]
-	result.append(temp)
+try:
+	for item in range(len(bytekey)):
+		print(bytetxt[item])
+		print(bytekey[item])
+		temp = (ord(bytetxt[item]) + ord(bytekey[item])) % 256
+		#print(temp)
+		result.append(temp)
+except:
+	print("please have the correct length between keyfile and plaintext");
+	exit();
 
 ciphertxt = open(cipherfile, "w")
-ciphertxt.write(''.join(str(result)))
-print(result)
+# ciphertxt.write(''.join(str(result)))
+ciphertxt.write(str(result))
+print(str(result))
 
 ciphertxt.close()
 keytxt.close()

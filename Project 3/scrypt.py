e56040d0d85f71d7cd4e7e8621b2867cc4fff272
jaycariaga <jaymcariaga@gmail.com>
2020-10-30 10:11:10 -0400

    first stage of pt 2 done

--- Project 3/scrypt.py
+++ Project 3/scrypt.py
@@ -1,3 +1,20 @@
 from sys import argv;
 #Jason Cariaga
 #171001720
+
+
+
+x = [0, 1, 2, 3]
+mybyte = bytearray(b'')
+for val in x:
+	a = 1103515245
+	m = 256
+	c = 12345
+	if not x:
+		break;
+	elif val == 0:
+		mybyte.append(x[val])
+		continue;
+	mybyte.append((a*x[val-1] + c) % m);
+
+print(mybyte)

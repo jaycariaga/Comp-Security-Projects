59904b87986abb2a7fb2c246801935354ab2951a
jaycariaga <jaymcariaga@gmail.com>
2020-11-01 12:02:56 -0500

    edits in pt 2

--- Project 3/scrypt.py
+++ Project 3/scrypt.py
@@ -1,10 +1,22 @@
 from sys import argv;
 #Jason Cariaga
 #171001720
+#for reading the keyfile
+def read_file(filename):
+	with open(filename, "rb") as f:
+		data = f.read()
+		return data; #currData is binary data of keyfile
 
+def sdbm(input):
+	hash = 0;
+	for val in input:
+		c = input[val]
+		hash = c + (hash * (2**6)) + (hash*(2**16)) - hash;
+	return hash;
 
-
+#sample input
 x = [0, 1, 2, 3]
+#returns bytearray of a series of bytes
 mybyte = bytearray(b'')
 for val in x:
 	a = 1103515245
@@ -18,3 +30,13 @@ for val in x:
 	mybyte.append((a*x[val-1] + c) % m);
 
 print(mybyte)
+
+try:
+	password = argv[1]
+	plainfile = argv[2];
+	cipherfile = argv[3];
+	key = read_file(keyfile)
+	#print(key)
+except:
+	print("please enter in names for password, plaintext, ");
+	exit();

d5a3a119dad299a97894bedb2b7069134d58d0fa
jaycariaga <jaymcariaga@gmail.com>
2020-11-11 18:20:39 -0500

    halfway done with pt 1

--- Project 4/pow-create.py
+++ Project 4/pow-create.py
@@ -4,9 +4,24 @@ import sys;
 #Jason Cariaga
 #171001720
 #done using Python 3.8
-
 import hashlib
 
-m = hashlib.sha256()
-m.update(b" goteeem")
-print(m.digest())
+try:
+	bits = argv[1]
+	message = argv[2]
+except: 
+	print("Please fill in all arguments: [bits] [output]")
+	exit()
+
+with open(message, "rb") as msg:
+	print ("File: {}".format(message))
+	while True:
+		data = msg.read()
+		if not data:
+			break
+		#what happens after reading entire file in binary
+		m = hashlib.sha256()
+		m.update(data)
+		computed = m.hexdigest()
+		#all works above
+		print("Initial-hash: {}".format(computed))

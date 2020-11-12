6693b75dfa5e81ebd7d01a2f02a41088fdc010d1
jaycariaga <jaymcariaga@gmail.com>
2020-11-12 11:32:24 -0500

    almost there

--- Project 4/pow-create.py
+++ Project 4/pow-create.py
@@ -5,23 +5,70 @@ import sys;
 #171001720
 #done using Python 3.8
 import hashlib
+import random
+import string
 
+
+def checkme(firsthash, sechash):
+	
+
+
+
+	return True;
+
+
+def prefixgen():
+	length = random.randint(1,7)
+	allchars = string.digits + string.ascii_letters + string.punctuation
+	allchars = allchars.replace('"', '')
+	allchars = allchars.replace("'", '')
+	allchars = allchars.replace(' ', '')
+	print(allchars)
+	result = ''.join(random.choice(allchars) for x in range(length))
+	return result
+
+
+limit = 10000000000 #amount of iterations before cutoff saying too much time
+current = 0
 try:
-	bits = argv[1]
+	difficulty = int(argv[1])
 	message = argv[2]
 except: 
 	print("Please fill in all arguments: [bits] [output]")
 	exit()
+if difficulty < 0:
+	print("Please have difficulty above 0:")
+	exit();
 
 with open(message, "rb") as msg:
 	print ("File: {}".format(message))
 	while True:
 		data = msg.read()
+		print(data)
 		if not data:
 			break
 		#what happens after reading entire file in binary
-		m = hashlib.sha256()
-		m.update(data)
-		computed = m.hexdigest()
+		first = hashlib.sha256()
+		first.update(data) #places data in hash function
+		computed = first.hexdigest() #converts message to hash 
 		#all works above
 		print("Initial-hash: {}".format(computed))
+
+		while True:
+			result = prefixgen().encode('ascii') #rand string converted to binary
+			tryme = result + data
+
+			sechash = hashlib.sha256()
+			sechash.update(tryme)
+			seccomp = sechash.hexdigest()
+
+			if not checkme(computed, seccomp): #when leading zeros is no success repeat again
+				continue;
+
+			print("sechash is: {}".format(seccomp))
+			print("computed is: {}".format(computed))
+			break;
+
+		# res = computed.rjust(difficulty + len(computed), '0') 
+		# print(res)
+
